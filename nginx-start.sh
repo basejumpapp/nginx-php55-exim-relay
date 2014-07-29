577561d66d335dbb7251639ea56c2619d0a8d3ea
Devon Weller <dweller@devonweller.com>
2014-07-29 11:46:52 -0500

    fixed nginx-start script

--- nginx-start.sh
+++ nginx-start.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 # get hostname
-if [ -n "$SERVER_NAME" ]; then
-    # use HOST_DOMAIN environment variable
-    PUBLIC_HOSTNAME=$SERVER_NAME
-else
+if [ -z "$SERVER_NAME" ]; then
     # last resort: just use the IP address
-    PUBLIC_HOSTNAME=$(hostname -I | cut -f1 -d' ')
+    SERVER_NAME=$(hostname -I | cut -f1 -d' ')
 fi
 
 # modify nginx.conf
 SRC_FILE="/etc/nginx/conf.d/default.template"
 DEST_FILE="/etc/nginx/conf.d/default.conf"
-SED_LINE1='s!@@DEFAULT_SERVER_NAME@@!'${PUBLIC_HOSTNAME}'!g'
+SED_LINE1='s!@@DEFAULT_SERVER_NAME@@!'${SERVER_NAME}'!g'
 echo $SED_LINE1 > /tmp/SED_LINE1;
 cat $SRC_FILE | sed -f /tmp/SED_LINE1 > $DEST_FILE
 
